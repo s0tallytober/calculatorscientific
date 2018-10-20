cd6814b3491b2eac65f9cc85996e015bf65e4cc1
venkateshk <s0tallytober>
2018-10-19 23:17:04 -0400

    Initial Commit

---
+++ sceintificcalci/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sceintificcalci
+//
+//  Created by Venkatesh K on Saka 1940-07-27.
+//  Copyright © 1940 Saka Venkatesh K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
